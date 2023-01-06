f0cb7a12c34e78cdca824799f7ed1e02e6d9aa33
miki134 <miki.n.2001@gmail.com>
2023-01-06 14:10:15 +0100

    added icon

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     MainWindow w;
     w.setWindowTitle("Chess");
-    //w.showMaximized();
+    w.setWindowIcon(QIcon(":/icons/icon.png"));
     w.show();
+
     return a.exec();
 }
